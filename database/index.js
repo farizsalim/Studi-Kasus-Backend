2e23ff7a1365f7392c70e16ff3e9db6c014427c6
farizsalim <farizsalim10@yahoo.co.id>
2023-12-17 23:22:14 +0800

    Perubahan url mongo

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
 
-mongoose.connect(`mongodb://test:fariz1009@0.0.0.0:27017/?authSource=admin`);
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?authSource=admin`);
 const db =  mongoose.connection;
 
 module.exports = db;
